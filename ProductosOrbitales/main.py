a50609e190b4456427680a2ac5b6d47dac881cdf
mcvalenti <mcvalenti717@hotmail.com>
2017-08-23 16:59:35 -0300

    Coordenadas Geodesicas

--- ProductosOrbitales/main.py
+++ ProductosOrbitales/main.py
@@ -27,7 +27,8 @@ def calcula_pasada(sat,obs,startTime,stopTime):
             obs.sitio.date=pass_time
             rise_time,rise_azimuth,max_elev_time,max_elev,set_time,set_azimuth=obs.sitio.next_pass(sat.sat)
             pass_duration=(set_time-rise_time)*1440
-            print rise_time.datetime().strftime('%Y-%m-%d %H:%M:%S.%f'),rise_azimuth,max_elev_time,max_elev,set_time,set_azimuth, pass_duration
+            print rise_time.datetime().strftime('%Y-%m-%d %H:%M:%S.%f'),rise_azimuth,max_elev_time,
+            max_elev,set_time,set_azimuth, pass_duration
             writer.writerow({'Pasada':n,'rise_time':rise_time.datetime().strftime('%Y-%m-%d %H:%M:%S.%f'),'rise_azimuth':rise_azimuth/degree,'max_elev_time':max_elev_time.datetime().strftime('%Y-%m-%d %H:%M:%S.%f'),'max_elev':max_elev/degree,'set_time':set_time.datetime().strftime('%Y-%m-%d %H:%M:%S.%f'),'set_azimuth':set_azimuth/degree,'duracion':pass_duration}) #csv
             rise_time_list.append(rise_time)
             set_time_list.append(set_time)
@@ -62,8 +63,7 @@ def calcula_eclipse(sat,obs,startTime,stopTime):
             prop_time=prop_time+timedelta(seconds=1)
     return {}
 
-def calcula_track(sat,startTime,stopTime):
-    
+def calcula_track(sat,startTime,stopTime):  
     prop_time=startTime.datetime()
     lon_lat_track=open('validaciones/lon_lat.dat','w')
     set_datos=[]
@@ -75,11 +75,13 @@ def calcula_track(sat,startTime,stopTime):
         writer.writeheader()       #csv 
         while prop_time < stopTime.datetime():
             sat.sat.compute(prop_time)
-            writer.writerow({'Epoca': prop_time.strftime('%Y-%m-%d %H:%M:%S'), 'Longitud': sat.sat.sublong/degree, 'Latitud':sat.sat.sublat/degree}) #csv
+            writer.writerow({'Epoca': prop_time.strftime('%Y-%m-%d %H:%M:%S'),
+                              'Longitud': sat.sat.sublong/degree,
+                               'Latitud':sat.sat.sublat/degree}) #csv
             lon_lat_track.write(str(sat.sat.sublong/degree)+' '+str(sat.sat.sublat/degree)+'\n')
             lat.append(sat.sat.sublat/degree)
             lon.append(sat.sat.sublong/degree)
-            prop_time=prop_time+timedelta(seconds=1) 
+            prop_time=prop_time+timedelta(seconds=1)  
     lon_lat_track.close()
     set_datos=[lon,lat]
     return set_datos
@@ -109,8 +111,22 @@ def grafica_track_pasada(sitio_lon,sitio_lat,set_datos_lista):
     for m in range(len(set_datos_lista)):
         x, y = map(set_datos_lista[m][0],set_datos_lista[m][1])
         map.scatter(x,y,2,marker='o',color='red')
-    plt.title('Mapitas')
+    plt.title('Proyeccion')
     plt.show()
+    
+def coord_Geodesicas(sat,prop_time,stopTime):
+    
+    with open('validaciones/coordGeod_noaa.csv', 'w') as csvfile: #csv
+        fieldnames = ['Epoca','Longitud','Latitud'] #csv 
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)   #csv 
+        writer.writeheader()       #csv 
+        while prop_time < stopTime:
+            sat.sat.compute(prop_time)
+            writer.writerow({'Epoca': prop_time.strftime('%Y-%m-%d %H:%M:%S'),
+                              'Longitud': sat.sat.sublong/degree,
+                               'Latitud':sat.sat.sublat/degree}) #csv            
+            prop_time=prop_time+timedelta(minutes=1)
+    return {}
 
 if __name__=='__main__':
 
@@ -122,14 +138,17 @@ if __name__=='__main__':
     #====================
     # Periodo de Analisis
     #====================
-    startTime=datetime(2017,1,1,0,0,0)
-    stopTime=datetime(2017,1,2,0,0,0)
+#     startTime=datetime(2017,3,8,0,0,0)
+#     stopTime=datetime(2017,3,9,0,0,0)
+    startTime=datetime(1991,1,1,0,0,0)
+    stopTime=datetime(1991,1,1,23,59,59)
     
     #====================
     # Tle satelite SAC-D
     #====================
 #    tle_archivo='tles/SACD_8_3_2017.tle'
     tle_archivo='tles/25544_enero_2017.tle'
+    tle_archivo='tles/noaa10_16969_1_1_1991.tle'
     tle=Tle.creadoxArchivo(tle_archivo)
     
     #====================
@@ -146,7 +165,7 @@ if __name__=='__main__':
     #==============================================================
     # PASADAS
     #==============================================================
-#    rise_time_list, set_time_list=calcula_pasada(sat, obs, startTime, stopTime)
+ #   rise_time_list, set_time_list=calcula_pasada(sat, obs, startTime, stopTime)
     
     #==============================================================
     # Tracks de pasadas
@@ -159,13 +178,21 @@ if __name__=='__main__':
 #         set_datos_lista.append(set_datos)
 #         n=n+1
 #     grafica_track_pasada(sitio_lon,sitio_lat,set_datos_lista)
+    
+    #==============================================================
+    # Coordenadas Geodesicas
+    #==============================================================
+    coord_Geodesicas(sat,startTime,stopTime)
+    
+    
     #==============================================================
     # Eclipses
     #==============================================================
-    calcula_eclipse(sat, obs, startTime, stopTime)
+#    calcula_eclipse(sat, obs, startTime, stopTime)
+
+
+
     
-
-
 
 
         
